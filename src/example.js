da368412f7624d01ac0c724beed5f46609fbcc6c
Grzegorz Pabian <greg.pabian@gmail.com>
2014-11-19 13:27:25 +0100

    Simulate asynchronous load.

--- src/example.js
+++ src/example.js
@@ -2,11 +2,17 @@ CKEDITOR.define( function() {
 	'use strict';
 
 	return {
-		init: function( editor ) {
-			console.log( 'init example for', editor );
-			editor.addCommand( 'example', function( arg ) {
-				console.log( 'example command called with:', arg );
-			} );
+		init: function( editor, done ) {
+			console.log( 'start init example for', editor );
+
+			// simulate an async initialization
+			setTimeout( function() {
+				console.log( 'done init example for', editor );
+				editor.addCommand( 'example', function( arg ) {
+					console.log( 'example command called with:', arg );
+				} );
+				done();
+			}, 1000 );
 		}
 	};
 } );
